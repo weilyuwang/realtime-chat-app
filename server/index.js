91ab554ce5aaeec628a5800fb934fe2ade6f2956
weilyuwang <weilyuwang@gmail.com>
2020-10-16 18:25:24 -0400

    :bug: listen on socket connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 import express from "express";
 import socketio from "socket.io";
 import http from "http";
+import router from "./router.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(router);
+
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("We have a new connection");
+
+  socket.on("disconnect", () => {
+    console.log("User had left");
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
