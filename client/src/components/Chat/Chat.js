7baec23854e9a2f6a5272e92d75b3e73da974b27
weilyuwang <weilyuwang@gmail.com>
2020-10-16 20:44:59 -0400

    :bug: client side socket setup

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+let socket;
+const ENDPOINT = "localhost:5000";
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+    // location prop comes from react-router-dom
+    // location.search: '?name=...&room=...'
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room });
+  }, [location]);
+
+  return <div>Chat</div>;
+};
+
+export default Chat;
