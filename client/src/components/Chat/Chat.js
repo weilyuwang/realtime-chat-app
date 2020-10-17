ea5f196375797d93df856a21496d3709f57f60b0
weilyuwang <weilyuwang@gmail.com>
2020-10-16 22:55:47 -0400

    :ambulance: update server endpoint

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,7 +8,7 @@ import Messages from "../Messages/Messages";
 import TextContainer from "../TextContainer/TextContainer";
 
 let socket;
-const ENDPOINT = "localhost:5000";
+const ENDPOINT = "https://weilyu-chat-app.herokuapp.com/";
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
